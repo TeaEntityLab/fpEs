b0096ff74957d9821166111355393e2764b24218
John Lee <johnteee@gmail.com>
2018-06-04 18:27:58 +0800

    Add basic monad defs.

--- src/monad.js
+++ src/monad.js
@@ -1,6 +1,34 @@
-class Monad {
-  ;
+class MonadDef {
+  constructor(ref) {
+    this.ref = ref;
+  }
+
+  isNull() {
+    return this.ref === undefined || this.ref === null;
+  }
+  isPresent() {
+    return ! this.isNull();
+  }
+
+  or(ref) {
+    return this.isPresent() ? this : new MonadDef(ref);
+  }
+  letDo(fn) {
+    if (this.isPresent()) {
+      fn();
+    }
+  }
+
+  unwrap() {
+    return this.ref;
+  }
 }
+
+var Monad = {};
+Monad.just = function (ref) {
+  var m = new MonadDef(ref);
+  return m;
+};
 
 module.exports = {
 	Monad,
--- test/monad.js
+++ test/monad.js
@@ -2,6 +2,55 @@ var {Monad} = require('../src/monad');
 
 describe('Monad', () => {
   it('New', () => {
-			let m = new Monad();
+			var m = Monad.just(1);
+	});
+  it('isPresent', () => {
+			var m;
+
+      m = Monad.just(1);
+      m.isPresent().should.equal(true)
+      m.isNull().should.equal(false)
+      m = Monad.just(null);
+      m.isPresent().should.equal(false)
+      m.isNull().should.equal(true)
+      m = Monad.just(undefined);
+      m.isPresent().should.equal(false)
+      m.isNull().should.equal(true)
+	});
+  it('or', () => {
+			var m;
+
+      m = Monad.just(1);
+      m.or(3).unwrap().should.equal(1)
+      m.or(4).unwrap().should.equal(1)
+      m = Monad.just(null);
+      m.or(3).unwrap().should.equal(3)
+      m.or(4).unwrap().should.equal(4)
+      m = Monad.just(undefined);
+      m.or(3).unwrap().should.equal(3)
+      m.or(4).unwrap().should.equal(4)
+	});
+  it('letDo', () => {
+			var m;
+      var v;
+
+      m = Monad.just(1);
+      v = 0;
+      m.letDo(function () {
+        v = 1;
+      });
+      v.should.equal(1)
+      m = Monad.just(null);
+      v = 0;
+      m.letDo(function () {
+        v = 1;
+      });
+      v.should.equal(0)
+      m = Monad.just(undefined);
+      v = 0;
+      m.letDo(function () {
+        v = 1;
+      });
+      v.should.equal(0)
 	});
 })
