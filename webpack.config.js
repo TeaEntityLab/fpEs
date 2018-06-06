582a5734a92191fe583f3551d00b3214970db77f
John Lee <johnteee@gmail.com>
2018-06-06 14:46:40 +0800

    Add bundled minified bundle.min.js. Improve build scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
-module.exports = {
-  entry: './index.js',
-  output: {
-      filename: './bundle.js'
-  },
-  mode: 'production',
-  module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: [
-                'babel-loader',
-              ],
-          },
-      ]
-  },
+module.exports = (env, argv) => {
+  return {
+    entry: './index.js',
+    output: {
+        filename: argv.mode === 'production' ? './bundle.min.js' : './bundle.js',
+    },
+    mode: argv.mode === 'production' ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                  'babel-loader',
+                ],
+            },
+        ]
+    },
+  };
 };
