c341f724b24697f9e168a7b51976e95142e8ed20
JunYi JohnTeee Lee <johnteee@gmail.com>
2018-06-15 21:29:09 +0800

    Add generatorToPromise()

--- monadio.js
+++ monadio.js
@@ -40,7 +40,7 @@ MonadIO.fromPromise = function (p) {
 MonadIO.promiseof = function (ref) {
   return Promise.resolve(ref);
 };
-MonadIO.doM = function (genDef) {
+MonadIO.wrapGenerator = function (genDef) {
   var gen = genDef();
   function step(value) {
       var result = gen.next(value);
@@ -52,7 +52,13 @@ MonadIO.doM = function (genDef) {
       }
       return result.value.then(step);
   }
-  return step();
+  return step;
+};
+MonadIO.generatorToPromise = function (genDef) {
+  return Promise.resolve().then(MonadIO.wrapGenerator(genDef));
+}
+MonadIO.doM = function (genDef) {
+  return MonadIO.wrapGenerator(genDef)();
 };
 
 module.exports = MonadIO;
--- test/monadio.js
+++ test/monadio.js
@@ -1,6 +1,6 @@
 import Monad from '../monad';
 import MonadIO from '../monadio';
-var {promiseof, doM} = MonadIO;
+var {promiseof, doM, generatorToPromise} = MonadIO;
 
 describe('MonadIO', function () {
   it('sync', function () {
@@ -36,6 +36,22 @@ describe('MonadIO', function () {
 
       v = 0;
       p = doM(function *() {
+        var value = yield promiseof(5);
+        var value2 = yield promiseof(11);
+        var value3 = yield Monad.just(3);
+        var value4 = yield MonadIO.just(3);
+        var value5 = yield MonadIO.fromPromise(Promise.resolve(3));
+        return value + value2 + value3 + value4 + value5;
+      });
+
+      return p.then((v)=>v.should.equal(25));
+	});
+  it('generatorToPromise', function () {
+      var p = undefined;
+			var v = 0;
+
+      v = 0;
+      p = generatorToPromise(function *() {
         var value = yield promiseof(5);
         var value2 = yield promiseof(11);
         var value3 = yield Monad.just(3);
