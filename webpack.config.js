d7b6d58fabc88ff41412eaac49a9fccdb0d98a6c
John Lee <johnteee@gmail.com>
2020-12-15 13:36:37 +0800

    Add BrotliPlugin for better compressions

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,24 @@
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+const zopfli = require('@gfx/zopfli')
+const BrotliPlugin = require('brotli-webpack-plugin')
+
+const dynamicPluginList = [
+  new CompressionWebpackPlugin({
+    algorithm(input, compressionOptions, callback) {
+      return zopfli.gzip(input, compressionOptions, callback)
+    },
+    compressionOptions: {
+      numiterations: 15
+    },
+    minRatio: 0.99,
+    test: productionGzipExtensions
+  }),
+  new BrotliPlugin({
+    test: productionGzipExtensions,
+    minRatio: 0.99
+  })
+]
 
 module.exports = (env, argv) => {
   return {
@@ -20,8 +40,6 @@ module.exports = (env, argv) => {
         ]
     },
     target: ['es5'],
-    plugins: [
-      new CompressionPlugin({}),
-    ],
+    plugins: dynamicPluginList,
   };
 };
